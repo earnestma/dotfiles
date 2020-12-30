ef380850164a2e20872945e6e827bda9ede93d0f
earnest ma <me@earne.link>
2020-12-30 13:29:51 -0500

    systemd/windows task scheduler no

--- bin/gitsync-dirs-win-wrapper.bat
+++
@@ -1,1 +0,0 @@
-cmd /c ""C:\Program Files\Git\bin\bash.exe" --login -i -- C:\Users\earne\bin\gitsync-dirs.sh"
--- bin/gitsync-dirs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "==========================" >> ~/.gs.log
-date >> ~/.gs.log
-
-cd ~/org
-pwd >> ~/.gs.log
-git sync >> ~/.gs.log
-
